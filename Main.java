3643241716abc88398b52dd0ab149a58f10e4e3c
dharitree-066 <166328366+Lalalili12368@users.noreply.github.com>
2026-05-23 22:47:33 +0600

    Add files via upload
    i have fixed the errors that were showing previously

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 import java.util.*;
-
+import java.nio.charset.StandardCharsets;
 public class Main {
 
         public static void main(
                         String[] args) {
+                                System.setOut(new java.io.PrintStream(System.out, true, StandardCharsets.UTF_8));
 
                 // ---------- START CODE GENERATION ----------
 
